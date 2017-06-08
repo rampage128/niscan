5eff190e9084191d45d6ebb863f131b18eb27444
rampage128 <frederik.wolter@googlemail.com>
2017-06-08 17:07:19 +0200

    clean up comments

--- niscan.ino
+++ niscan.ino
@@ -7,35 +7,34 @@
 #include "niscan_dfs.h"
 #include "niscan.h"
 
-#define CAN0_INT 2  // Set INT to pin 2
-MCP_CAN CAN0(10);   // Set CS to pin 10
+#define CAN0_INT 2
+MCP_CAN CAN0(10);
 
 ClimateControl climate;
 NissanClimateControlPopulator climateControlPopulator(&climate);
 
 void setup() {
   Serial.begin(115200);
-  
-  // Initialize MCP2515 running at 8MHz with a baudrate of 500kb/s and the masks and filters disabled.
-  if (CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) {
-    // Set operation mode to normal so the MCP2515 sends acks to received data.
-    CAN0.setMode(MCP_NORMAL);                     
 
-    // Configuring pin for /INT input
+  // Initialize MCP2515 module
+  if (CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) {
+    CAN0.setMode(MCP_NORMAL);                     
     pinMode(CAN0_INT, INPUT);
     Serial.write(">ardy|");
   }
+  // Notify serial on error
   else {
     Serial.write(">e");
     Serial.write(0x01);
     Serial.write("|");
   }
 
+  // Make sure we sent all serial data, before entering the loop
   Serial.flush();
 }
 
 void loop() {
-  // If CAN0_INT pin is low, read receive buffer
+  // Read can-bus data
   if (!digitalRead(CAN0_INT)) { 
     CanPacket package = CanPacket::fromMcp(CAN0);
     climateControlPopulator.populate(&package);
